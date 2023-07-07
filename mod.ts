d1c0f0b9138afb6d130d7ffc6b6ccc7d73910f99
Tyler Mueller <muelltyl@gmail.com>
2023-07-07 13:57:58 -0500

    adds JSDocs and renames some variables

--- mod.ts
+++ mod.ts
@@ -1,57 +1,114 @@
 // deno-lint-ignore-file no-explicit-any
 
+/**
+ * A custom class that extends Error that allows for namable Errors inside
+ * the type system.
+ *
+ * ```
+ * const myError = new Err("MyError");
+ * // myError: Err<"MyError">
+ * ```
+ */
 export class Err<N extends string = "Error"> extends Error {
   name!: N;
   cause?: Err;
-  constructor(name?: N, message?: string) {
-    super(message);
+  constructor(name?: N, message?: string, cause?: Error) {
+    super(message, {
+      cause,
+    });
     if (name) {
       this.name = name;
     }
   }
 }
 
+/**
+ * A union type between a return type `T` and an Error type `E`.
+ */
 export type Result<T, E extends Error | Err = Err<string>> = T | E;
 
-export type ExtractName<T> = T extends { name: infer N } ? N : never;
+type ExtractName<T> = T extends { name: infer N } ? N : never;
 
+/**
+ * Returns whether a `Result` is an instance of an Error. If a `name` is provided,
+ * checks if the Error's name or it's causes contain an Error with that name.
+ *
+ * ```
+ * const result = await CaptureErr("Fetch Error", async (): MyStruct => {
+ *  const res = await fetch(url);
+ *  return await res.json();
+ * });
+ * if(isErr(result, "Fetch Error")) {
+ *  // result: Err<"Fetch Error">
+ * } else {
+ *  // result: MyStruct
+ * }
+ * ```
+ */
 export function isErr<R extends Result<any>, N extends ExtractName<R> & string>(
-  value: R,
+  result: R,
   name?: N,
-): value is Extract<R, Err<N>> & Extract<R, Error> {
-  if (!(value instanceof Error)) return false;
+): result is Extract<R, Err<N>> & Extract<R, Error> {
+  if (!(result instanceof Error)) return false;
   if (name === undefined) return true;
-  if (value.name === name) return true;
-  if (value.cause instanceof Error) {
-    return isErr(value.cause, name);
+  if (result.name === name) return true;
+  if (result.cause instanceof Error) {
+    return isErr(result.cause, name);
   }
   return false;
 }
 
+/**
+ * Returns the result if it's not an Error, otherwise returns `null`.
+ *
+ * The primary use of this function is with the nullish coalescence operator
+ * `??` in providing a value if the result ended up being an Error.
+ *
+ * ```
+ * function parseJSON(input: string) {
+ *  const json = CaptureErr("JSON Parse Error",
+ *    () => JSON.parse(input)
+ *  )
+ *  return Ok(json) ?? {}
+ * }
+ * ```
+ */
 export function Ok<
   R extends any,
 >(
-  value: Result<R>,
+  result: Result<R>,
 ): R | null {
-  if (isErr(value)) {
+  if (isErr(result)) {
     return null;
   }
-  return value;
+  return result;
 }
 
 type InnerType<T> = T extends Promise<infer U> ? U : never;
 
+/**
+ * Captures an Error in the given `callback` function and returns it as a
+ * `Result` with the specified Error `name`.
+ *
+ * ```
+ * // result: MyStruct | Err<"Fetch Error">
+ * const result = await CaptureErr("Fetch Error", async (): MyStruct => {
+ *  const res = await fetch(url);
+ *  return await res.json();
+ * });
+ * ```
+ */
 export function CaptureErr<
   F extends (...args: any[]) => any,
   N extends string,
   R extends ReturnType<F>,
 >(
   name: N,
-  fn: F,
+  callback: F,
   message?: string,
 ): R extends Promise<any> ? Promise<InnerType<R> | Err<N>> : R | Err<N> {
   try {
-    const result = fn();
+    const result = callback();
     if (result instanceof Promise) {
       return result.catch((e: Error) => {
         e.cause = new Err(name ?? e.name, message ?? e.message);
@@ -67,6 +124,22 @@ export function CaptureErr<
   }
 }
 
+/**
+ * Unwraps a `Result`, throwing the Error if one exists.
+ *
+ * This should only be used
+ * when the given Error cannot be resolved gracefully or you are completely
+ * sure the `Result` will not produce an Error.
+ * ```
+ * const result = await CaptureErr("Fetch Error", async (): MyStruct => {
+ *  const res = await fetch(url);
+ *  return await res.json();
+ * });
+ * // ^ result: MyStruct | Err<"Fetch Error"
+ * const json = UnwrapErr(result);
+ * // ^ json: MyStruct
+ * ```
+ */
 export function UnwrapErr<T>(result: Result<T>): T {
   if (result instanceof Error) {
     throw result;
